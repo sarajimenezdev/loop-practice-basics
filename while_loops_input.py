968be6b7dbc8c4ee957c18fe67824636db8cfb50
sarajimenezdev <sara.jimenezs@outlook.com>
2026-04-27 18:12:37 -0500

    Add basic for loop exercises
    Updated task numbers in comments for clarity.

--- while_loops_input.py
+++ while_loops_input.py
@@ -1,4 +1,4 @@
-#5. Write a program that asks the user to enter a name, to print 'Hello' followed by the name, and to keep asking until the user enters a non-empty name.
+#1. Write a program that asks the user to enter a name, to print 'Hello' followed by the name, and to keep asking until the user enters a non-empty name.
 
 name = input('¿Cuál es tu nombre? ')
 while name == '':
@@ -6,7 +6,7 @@ while name == '':
     name = input('¿Cuál es tu nombre? ')
 print(f'Hola {name}')
 
-#6. Write a program that asks the user to enter their age, to print 'You are [age] years old', and to keep asking until the user enters a non-negative age.
+#2. Write a program that asks the user to enter their age, to print 'You are [age] years old', and to keep asking until the user enters a non-negative age.
 
 age = int(input('¿Cuantos años tienes? '))
 while age < 0:
@@ -14,7 +14,7 @@ while age < 0:
     age = int(input('¿Cuantos años tienes? '))
 print(f'Tienes {age} años')
 
-# 7. Write a program that asks the user to enter a positive number, to print the multiplication table for that number from 1 to 10, and to keep asking until the user enters a positive number.
+# 3. Write a program that asks the user to enter a positive number, to print the multiplication table for that number from 1 to 10, and to keep asking until the user enters a positive number.
 num = int(input('Enter a positive number: '))
 while num < 0:
     print('That was not a positive number.')
@@ -22,7 +22,7 @@ while num < 0:
 if num > 0:
     for i in range (1, 11):
         print(f'{num} x {i} = {num * i}')
-#8. Write a program that asks the user to enter numbers until they enter 0 and then displays the total sum.
+#4. Write a program that asks the user to enter numbers until they enter 0 and then displays the total sum.
 
 number = int(input('Enter any number from 0 to 100: '))
 total_sum = 0
